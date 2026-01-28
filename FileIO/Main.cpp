78c516bd7e183cdf1863cc7201568cef8fe8f6f6
No-Jyun <hj010410@gmail.com>
2026-01-28 17:23:50 +0900

    파일 입출력 프로젝트 추가
    - c 스타일 파일 입출력 수업 진행

---
+++ FileIO/Main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+int main()
+{
+	FILE* file = nullptr;
+	fopen_s(&file, "Test.txt", "rt");
+
+	// 파일 읽기 실패
+	if (file == nullptr)
+	{
+		std::cout << "Failed to read file\n";
+		__debugbreak;
+	}
+
+	// 파일 쓰기
+	//const char* message = "프로그램에서 작성한 문자열 값입니다.";
+	//fputs(message, file);
+
+	// 파일 읽기
+	//char data[256] = { };
+	//while (true)
+	//{
+	//	if (fgets(data, 256, file) == nullptr)
+	//	{
+	//		break;
+	//	}
+	//	
+	//	std::cout << data;
+	//}
+
+	// 파일 크기 가늠
+	fseek(file, 0, SEEK_END);
+	size_t fileSize = ftell(file);
+
+	std::cout << "FileSize: " << fileSize << "\n";
+
+	// ! 파일 크기 가늠한 뒤에 다시 처음부터 읽으려면 FP 되돌려야함!!
+	//fseek(file, 0, SEEK_SET);
+	rewind(file);
+
+	// 블록 단위로 읽기
+	// 읽은 데이터를 저장하기 위한 공간
+	char buffer[1024] = {};
+	size_t readSize = fread(buffer, sizeof(char), 1024, file);
+
+	std::cout << "readSize : " << readSize << "\n";
+	std::cout << buffer;
+
+	fclose(file);
+}
